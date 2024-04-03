0f7c888d8370a6aabb39648b75cbdc89e38e2a27
Relan <ignaciorelancio3@gmail.com>
2024-04-03 18:16:27 -0300

    prueba ts

---
+++ prueba.js
@@ -0,0 +1,2 @@
+var nombre = "Ignacio";
+console.log("El nombre de seba tendria que ser:" + nombre);
---
+++ prueba.ts
@@ -0,0 +1,4 @@
+let nombre: string = "Ignacio" ;
+
+console.log("El nombre de seba tendria que ser:" + nombre);
+
