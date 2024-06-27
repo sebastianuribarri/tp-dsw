5c2f76b3bfff3f85cc204e2de0feb14e711eb7e2
LucianoKomorovski <lucianokomorovski7@gmail.com>
2024-06-27 16:43:54 -0300

    standings into competition

--- backend/src/Competition/infrastructure/competition.schema.ts
+++ backend/src/Competition/infrastructure/competition.schema.ts
@@ -1,6 +1,14 @@
 import { Schema, model } from "mongoose";
+import Team from "../../Team/domain/team.entity.js";
+import { teamSchema } from "../../Team/infrastructure/team.schema.js";
 
 export const competitionSchema = new Schema({
+  standings: { type: [{team: teamSchema,
+     points: {type: Number, default: 0},
+     goalsDiff: {type: Number, default: 0}, 
+     group: {type: String, default: ''},
+     description: {type: String , default: ''},}],
+     default: []},
   id: { type: Number, require: true, default: 0 },
   start: { type: Date, require: true, default: new Date() },
   end: { type: Date, require: true, default: new Date() },
