5a83b95b9b36f6cf721eec1dd2538556b4303da4
= <franciscobuthet@gmail.com>
2024-09-18 13:24:05 -0300

    live matches

--- backend/src/Match/application/match.use_cases.ts
+++ backend/src/Match/application/match.use_cases.ts
@@ -5,6 +5,9 @@ import Competition from "../../Competition/domain/competition.entity.js";
 import EventUseCases from "../../Event/application/event.use_cases.js";
 import CompetitionUseCases from "../../Competition/application/competition.use_cases.js";
 import CompetitionsRoutes from "../../Competition/presentation/competition.routes.js";
+import LineUpUseCases from "../../LineUp/application/LineUp.use_cases.js";
+import LiveMatchesTimmer from "../domain/live_match.timmer.js";
+
 
 // const REGIONS = ["Argentina", "World"];
 const REGIONS = ["Argentina"];
@@ -14,6 +17,7 @@ export default class MatchUseCases {
     private readonly matchApiRepository: IApiRepository<Match>,
     private readonly matchDbRepository: IMatchRepository,
     private readonly eventUseCases: EventUseCases,
+    private readonly lineupUseCases: LineUpUseCases,
     private readonly competitionUseCases: CompetitionUseCases
   ) {}
 
@@ -39,6 +43,30 @@ export default class MatchUseCases {
     return false;
   }
 
+    public async liveMatchesNeedUpdate (){
+        console.log(
+      `LiveMatches ------------------------------------------------------------------------------`
+    );
+    const liveMatchesTimmer = await LiveMatchesTimmer.getInstance();
+
+    if (liveMatchesTimmer.liveMatchesUpdated()) return false;
+    
+    const competitions = await this.competitionUseCases.listAll();
+    let apiLiveMatches = await this.matchApiRepository.findAll({live:"all"});
+    apiLiveMatches = apiLiveMatches.filter((match)=>
+      competitions.find((competition)=>
+        competition.id === match.competition.id))
+
+    for(let liveMatch of apiLiveMatches){
+      const dbMatch = await this.matchDbRepository.findById(liveMatch.id)
+      if (!dbMatch) await this.createMatch(liveMatch);
+      else {      
+        dbMatch.updateMatch(liveMatch.date, liveMatch.status, liveMatch.goals)
+        await this.updateMatch(dbMatch);
+      }
+    }
+  }
+
   private async updateCompetitionMatches(
     matches: Match[],
     competition: Competition
@@ -49,8 +77,40 @@ export default class MatchUseCases {
     for (let match of matches) {
       const dbMatch = dbMatches.find((dbMatch) => dbMatch.id === match.id);
       if (!dbMatch) await this.createMatch(match);
-      else await this.updateMatch(match);
+      else {      
+        dbMatch.updateMatch(match.date, match.status, match.goals)
+        await this.updateMatch(dbMatch);
+      }
     }
+  }
+
+    public async getMatch (id: number){
+    let matchDetail = await this.matchDbRepository.findById(id);
+
+    let competitionMatches= await this.needUpdate(matchDetail.competition);
+    if (competitionMatches){
+      const match = competitionMatches.find((match_)=>match_.id===matchDetail.id)
+      matchDetail.updateMatch(match.date, match.status, match.goals)
+    }
+    //chequear si se esta jugando
+
+      const newEvents = await this.eventUseCases.needUpdate(
+      matchDetail
+    );
+    if (newEvents) {
+      matchDetail.events = newEvents;
+    }
+
+      const newLineUps = await this.lineupUseCases.needUpdate(
+      matchDetail
+    );
+    if (newLineUps) {
+      matchDetail.lineups = newLineUps;
+    }
+
+    if (newEvents || newLineUps || competitionMatches) this.matchDbRepository.updateOne(matchDetail.id, matchDetail)
+
+    return matchDetail;
   }
 
   // public async listAll() {
@@ -73,7 +133,6 @@ export default class MatchUseCases {
 
   public async listLiveMatches() {}
 
-  public async getMatch(id: number) {}
 
   private async updateMatch(match: Match) {
     let dbMatch = await this.matchDbRepository.updateOne(match.id, match);
