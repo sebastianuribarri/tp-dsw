3fd859bd60ec42e582f3b0baf10f6fdcf8d5a073
LucianoKomorovski <lucianokomorovski7@gmail.com>
2024-12-19 12:10:21 -0300

    route calendar

--- backend/src/Match/presentation/match.routes.ts
+++ backend/src/Match/presentation/match.routes.ts
@@ -7,6 +7,9 @@ export default class MatchRoutes {
   constructor(matchesController: MatchController, server: Express) {
     const matchesRouter = Router();
     server.use("/api/matches", matchesRouter);
+    matchesRouter.get("/calendar", (req, res, next) => {
+    authenticateUser(req, res, next, { premium: true });
+    } , matchesController.getCalendar);
     matchesRouter.use(authenticateUser);
     matchesRouter.get("/", matchesController.getAll);
     matchesRouter.get("/search", matchesController.getBySearch);
@@ -14,6 +17,6 @@ export default class MatchRoutes {
     matchesRouter.get("/teams", matchesController.getMatchesByTeams);
     matchesRouter.get("/live", matchesController.getLiveMatches);
     matchesRouter.get("/:id", matchesController.getOne);
-    matchesRouter.get("/calendar", matchesController.getCalendar);
+
   }
 }
