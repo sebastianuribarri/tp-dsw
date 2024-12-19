9ae884fe4d4cb4bbe3143dce6dd4d0d256bf2637
LucianoKomorovski <lucianokomorovski7@gmail.com>
2024-12-19 10:52:20 -0300

    frontend api axios

--- frontend/src/api/team.ts
+++ frontend/src/api/team.ts
@@ -1,27 +1,22 @@
 import API_URL, { getAuthHeaders } from ".";
+import axios from "axios";
+
 
 const TEAM_API_URL = API_URL + "teams/";
 
 export const getTeamById = async (id: number) => {
   const headers = getAuthHeaders();
-  return await fetch(TEAM_API_URL + String(id), {
-    method: "GET",
-    headers: headers,
-  });
-};
+  return await axios.get(`${TEAM_API_URL}${id}`, { headers }
+  );};
 
 export const getAllTeams = async () => {
   const headers = getAuthHeaders();
-  return await fetch(TEAM_API_URL, {
-    method: "GET",
-    headers: headers,
-  });
+  return await axios.get(`${TEAM_API_URL}`, { headers }
+  );
 };
 
 export const getTeamsBySearch = async (search: string) => {
   const headers = getAuthHeaders();
-  return await fetch(TEAM_API_URL + `search?search=${search}`, {
-    method: "GET",
-    headers: headers,
-  });
+  return await axios.get(`${TEAM_API_URL}${search}`, { headers }
+  );
 };
